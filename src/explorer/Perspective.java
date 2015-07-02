58b66fa00f70803c84e43e2621fd347a611f3481
Taewoo Kim <tu_k@naver.com>
2015-07-02 10:15:51 +0900

    init

--- src/explorer/Perspective.java
+++ src/explorer/Perspective.java
@@ -6,5 +6,9 @@ import org.eclipse.ui.IPerspectiveFactory;
 public class Perspective implements IPerspectiveFactory {
 
 	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+
+		layout.addStandaloneView(TestView.ID, false, IPageLayout.LEFT,
+				0.5f, layout.getEditorArea());
 	}
 }
